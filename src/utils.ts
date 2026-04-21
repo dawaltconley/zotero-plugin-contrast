72cda3893dbfff270c31ad49cde79602fac260ad
Dylan Awalt-Conley <web@dylan.ac>
2026-04-21 17:50:48 -0400

    fixup! further refactor plugin methods

--- src/utils.ts
+++ src/utils.ts
@@ -10,5 +10,19 @@ export const isSnapshotReader = (
   r: _ZoteroTypes.ReaderInstance,
 ): r is _ZoteroTypes.ReaderInstance<'snapshot'> => r.type === 'snapshot';
 
-export const isIframe = (e: Element): e is HTMLIFrameElement =>
+export const isIFrame = (e: Element): e is HTMLIFrameElement =>
   e.tagName.toUpperCase() === 'IFRAME';
+
+export async function waitForReader(
+  reader: _ZoteroTypes.ReaderInstance,
+): Promise<void> {
+  await Promise.all([reader._waitForReader(), reader._initPromise]);
+}
+
+export async function waitForInternalReader(
+  reader: _ZoteroTypes.ReaderInstance,
+): Promise<void> {
+  if (reader._internalReader) {
+    await reader._internalReader._primaryView.initializedPromise;
+  }
+}
